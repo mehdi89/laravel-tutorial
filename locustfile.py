8342b5b7c8fecf8b2c5eed0f38812d89f30d0136
mehedi hasan <hasanmehdi89@gmail.com>
2019-11-04 02:30:11 +0600

    new test cases

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,36 @@
 from locust import HttpLocust, TaskSet
 
-def index(l):
-    l.client.get("/")
+def login(l):
+    l.client.post("/admin/auth/login", {"username":"admin", "password":"education"})
 
-def stress(l):
-    l.client.get("/api/stress")
+def logout(l):
+    l.client.get("/admin/auth/logout")
+
+def index(l):
+    l.client.get("/admin")
+
+def users(l):
+    l.client.get("/admin/auth/users")
+
+def roles(l):
+    l.client.get("/admin/auth/roles")
+
+def logs(l):
+    l.client.get("/admin/auth/logs")
+
+def sleep(l): 
+    l.client.get('/api/sleep/5'); 
 
 class UserBehavior(TaskSet):
-    tasks = {index: 2, stress: 1}
+    tasks = {index: 1, users: 2, roles: 1, logs:2, sleep:1}
+
+    def on_start(self):
+        login(self)
+
+    def on_stop(self):
+        logout(self)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 5000
+    min_wait = 3000
     max_wait = 9000
